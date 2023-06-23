ea19b8852d1684c0b55d1264f39201dd87f64e9f
manumafe98 <manumaxerapro@gmail.com>
2023-06-23 17:44:30 -0300

    Correcting templates, adding routes, and  adding imports to make the endpoints serve html files

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import Fastapi
 from routers import routes
+from fastapi.staticfiles import StaticFiles
 
 app = Fastapi()
 
 app.include_router(routes.router)
+app.mount("/static", StaticFiles(directory="static"), name="static")
--- routers/routes.py
+++ routers/routes.py
@@ -1,6 +1,8 @@
 import os
 import requests
-from fastapi import APIRouter
+from fastapi import Request, Form, APIRouter, status
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
 from db.models.models import Movies, Users
 from db.schemas.schemas import MovieCreate, UserCreate
@@ -11,9 +13,32 @@ TBD_SEARCH_API = "https://api.themoviedb.org/3/search/movie"
 TBD_GET_API = "https://api.themoviedb.org/3/movie/"
 IMAGE_PATH = "https://image.tmdb.org/t/p/w500"
 
+
 router = APIRouter()
+templates = Jinja2Templates(directory="templates")
 
 
-router.get("/")
-async def home():
+router.get("/", response_class=HTMLResponse)
+async def home(request: Request, db: Session, skip: int = 0):
+    all_movies = db.query(Movies).offset(skip).order_by(Movies.rating.desc()).all()
+    return templates.TemplateResponse("index.html", {"request": request, "movies": all_movies})
+
+
+router.get("/edit", response_class=HTMLResponse)
+async def edit(request: Request):
+    return templates.TemplateResponse("edit.html", {"request": request})
+
+
+router.post("/edit", response_class=HTMLResponse)
+async def edit_formt(request: Request):
+    pass
+
+
+router.get("/add", response_class=HTMLResponse)
+async def add(request: Request):
+    return templates.TemplateResponse("add.html", {"request": request})
+
+
+router.post("/add", response_class=HTMLResponse)
+async def add_form(request: Request):
     pass
