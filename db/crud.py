1c81a7b47ad92b76fd8ae9acf2c1dcda206773e2
manumafe98 <manumaxerapro@gmail.com>
2023-06-24 17:37:20 -0300

    Adding crud helper functions and making the routes

---
+++ db/crud.py
@@ -0,0 +1,28 @@
+from sqlalchemy.orm import Session
+from db.models.models import Movies, Users
+from db.schemas.schemas import MovieCreate, UserCreate
+
+def get_all_movies(db: Session, skip: int = 0):
+    return db.query(Movies).offset(skip).order_by(Movies.rating.desc()).all()
+
+
+def get_movie_by_id(db: Session, movie_id: int):
+    return db.query(Movies).filter(Movies.id == movie_id).first()
+
+
+def get_movie_by_title(db: Session, movie_title: str):
+    return db.query(Movies).filter(Movies.title == movie_title).first()
+
+
+def create_movie_item(db: Session, movie: MovieCreate):
+    db_movie = Movies(**movie.dict())
+    db.add(db_movie)
+    db.commit()
+    db.refresh(db_movie)
+    return db_movie
+
+
+def delete_movie_item(db: Session, movie_id: int):
+    movie_to_delete = get_movie_by_id(db=db, movie_id=movie_id)
+    db.delete(movie_to_delete)
+    db.commit()
--- routers/routes.py
+++ routers/routes.py
@@ -1,11 +1,13 @@
 import os
 import requests
-from fastapi import Request, Form, APIRouter, status
+from fastapi import Request, Form, APIRouter, status, Depends
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
+from db.client import SessionLocal, engine, Base
 from db.models.models import Movies, Users
 from db.schemas.schemas import MovieCreate, UserCreate
+from db.crud import get_all_movies, get_movie_by_title, delete_movie_item, get_movie_by_id, create_movie_item
 
 # $env:API_KEY = "38d089808acd67f9a32d59629a4578a8"
 API_KEY = os.environ.get("API_KEY")
@@ -13,14 +15,28 @@ TBD_SEARCH_API = "https://api.themoviedb.org/3/search/movie"
 TBD_GET_API = "https://api.themoviedb.org/3/movie/"
 IMAGE_PATH = "https://image.tmdb.org/t/p/w500"
 
-
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
 
+Base.metadata.create_all(bind=engine)
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 
 router.get("/", response_class=HTMLResponse)
-async def home(request: Request, db: Session, skip: int = 0):
-    all_movies = db.query(Movies).offset(skip).order_by(Movies.rating.desc()).all()
+async def home(request: Request, db: Session = Depends(get_db), skip: int = 0):
+    all_movies = get_all_movies(db=db, skip=skip)
+    for movie in all_movies:
+        ranking = all_movies.index(movie) + 1
+        movie_to_edit = get_movie_by_title(db=db, movie_title=movie.title)
+        movie_to_edit.ranking = ranking
+        db.commit()
     return templates.TemplateResponse("index.html", {"request": request, "movies": all_movies})
 
 
@@ -29,25 +45,54 @@ async def edit(request: Request):
     return templates.TemplateResponse("edit.html", {"request": request})
 
 
-router.post("/edit", response_class=HTMLResponse)
-async def edit_formt(request: Request):
-    pass
+router.post("/edit/{movie_id}", response_class=HTMLResponse)
+async def edit_form(movie_id: int, request: Request, db: Session = Depends(get_db),
+                    rating: float = Form(...), review: str = Form(...)):
+    movie_to_update = get_movie_by_id(db=db, movie_id=movie_id)
+    movie_to_update.rating = rating
+    movie_to_update.review = review
+    db.commit()
+    redirect_url = router.url_path_for("home")
+    return RedirectResponse(redirect_url, status_code=status.HTTP_303_SEE_OTHER)
 
 
 router.get("/add", response_class=HTMLResponse)
 async def add(request: Request):
     return templates.TemplateResponse("add.html", {"request": request})
-
+    
 
 router.post("/add", response_class=HTMLResponse)
-async def add_form(request: Request):
-    pass
+async def add_form(request: Request, movie_title: str = Form(...)):
+    response = requests.get(TBD_SEARCH_API, params={"api_key": API_KEY, "query": movie_title})
+    output = response.json()
+    data = output["results"]
+    return templates.TemplateResponse("select.html", {"request": request, "matched_movies": data})
 
 
-router.get("/get_movie_data")
-def get_movie_data(request: Request):
-    movie_id = int(request.get("id"))
-    pass
+router.get("/delete/{movie_id}", response_class=HTMLResponse)
+async def delete(movie_id: int, db: Session = Depends(get_db)):
+    delete_movie_item(db=db, movie_id=movie_id)
+    redirect_url = router.url_path_for("home")
+    return RedirectResponse(redirect_url, status_code=status.HTTP_303_SEE_OTHER)
+
+
+router.get("/get_movie_data/{movie_id}", response_class=HTMLResponse)
+def get_movie_data(movie_id: int, request: Request, db: Session = Depends(get_db)):
+    movie_api_url = f"{TBD_GET_API}/{movie_id}"
+    response = requests.get(movie_api_url, params={"api_key": API_KEY, "language": "en-US"})
+    output = response.json()
+    new_record = MovieCreate(title=output["original_title"],
+                             description=output["overview"],
+                             year=output["release_date"].split("-")[0],
+                             img_url=f"{IMAGE_PATH}{output['poster_path']}",
+                             rating=1.0,
+                             ranking=1,
+                             review="")
+    create_movie_item(db=db, movie=new_record)
+    get_movie = get_movie_by_title(db=db, movie_title=output["original_title"])
+    get_id = get_movie.id
+    redirect_url = router.url_path_for("edit", movie_id=get_id)
+    return RedirectResponse(redirect_url, status_code=status.HTTP_303_SEE_OTHER)
 
 # TODO make the post enpoints work and the get_movie_data
 # TODO once the main app is working add register and login
