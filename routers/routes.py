a395c7aec8f53f82ad7631a18caa0dc6181025ef
manumafe98 <manumaxerapro@gmail.com>
2023-06-27 19:21:28 -0300

    Making the login work with fastapi-login

--- routers/routes.py
+++ routers/routes.py
@@ -1,17 +1,19 @@
 import os
 import requests
-from typing import Annotated
-from fastapi import Request, Form, APIRouter, status, Depends, Response
+from fastapi import Request, Form, APIRouter, status, Depends
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from db.schemas.schemas import MovieCreate, UserCreate, User
 from sqlalchemy.orm import Session
-from db.client import SessionLocal, engine, Base
+from db.client import engine, Base, SessionLocal
 from db.crud import get_all_movies, get_movie_by_title, delete_movie_item, get_movie_by_id, create_movie_item
 from db.crud import create_user, get_user_by_username
-from security.security import get_password_hash, verify_password, create_access_token, verify_token
+from fastapi.security import OAuth2PasswordRequestForm
+from fastapi_login import LoginManager
+from passlib.context import CryptContext
 
-# $env:API_KEY = "38d089808acd67f9a32d59629a4578a8"
+# API_KEY = "38d089808acd67f9a32d59629a4578a8"
+SECRET_KEY = "217e937592f0ea3c1975629980430a541c3c690b6c55fd8beff9a49bec86d16f"
 API_KEY = os.environ.get("API_KEY")
 TBD_SEARCH_API = "https://api.themoviedb.org/3/search/movie"
 TBD_GET_API = "https://api.themoviedb.org/3/movie/"
@@ -19,10 +21,11 @@ IMAGE_PATH = "https://image.tmdb.org/t/p/w500"
 
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
-
 Base.metadata.create_all(bind=engine)
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+manager = LoginManager(SECRET_KEY, "/user/signin", use_cookie=True)
 
-# Dependency
+
 def get_db():
     db = SessionLocal()
     try:
@@ -31,9 +34,21 @@ def get_db():
         db.close()
 
 
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+
+@manager.user_loader()
+def load_user(username: str, db: Session = next(get_db())):
+    return get_user_by_username(db=db, username=username)
+
+
 @router.get("/", response_class=HTMLResponse)
-async def home(current_user: Annotated[User, Depends(verify_token)], 
-               request: Request, db: Session = Depends(get_db)):
+async def home(request: Request, user = Depends(manager), db: Session = Depends(get_db)):
     all_movies = get_all_movies(db=db)
     for movie in all_movies:
         ranking = all_movies.index(movie) + 1
@@ -44,7 +59,7 @@ async def home(current_user: Annotated[User, Depends(verify_token)],
 
 
 @router.get("/edit/{movie_id}", response_class=HTMLResponse)
-async def edit(movie_id: int, request: Request):
+async def edit(movie_id: int, request: Request, user = Depends(manager)):
     return templates.TemplateResponse("edit.html", {"request": request})
 
 
@@ -59,7 +74,7 @@ async def edit_form(movie_id: int, request: Request, db: Session = Depends(get_d
 
 
 @router.get("/add", response_class=HTMLResponse)
-async def add(request: Request):
+async def add(request: Request, user = Depends(manager)):
     return templates.TemplateResponse("add.html", {"request": request})
     
 
@@ -72,13 +87,13 @@ async def add_form(request: Request, movie_title: str = Form(...)):
 
 
 @router.get("/delete/{movie_id}", response_class=HTMLResponse)
-async def delete(movie_id: int, db: Session = Depends(get_db)):
+async def delete(movie_id: int, db: Session = Depends(get_db), user = Depends(manager)):
     delete_movie_item(db=db, movie_id=movie_id)
     return RedirectResponse(router.url_path_for("home"), status_code=status.HTTP_303_SEE_OTHER)
 
 
 @router.get("/get_movie_data/{movie_id}", response_class=HTMLResponse)
-def get_movie_data(movie_id: int, request: Request, db: Session = Depends(get_db)):
+def get_movie_data(movie_id: int, request: Request, db: Session = Depends(get_db), user = Depends(manager)):
     movie_api_url = f"{TBD_GET_API}/{movie_id}"
     response = requests.get(movie_api_url, params={"api_key": API_KEY, "language": "en-US"})
     output = response.json()
@@ -121,18 +136,22 @@ def login(request: Request):
 
 
 @router.post("/user/signin", response_class=HTMLResponse)
-def login_form(request: Request, db: Session = Depends(get_db), 
-               username: str = Form(...), password: str = Form(...)):
-    user = get_user_by_username(db=db, username=username)
+def login_form(request: Request, db: Session = Depends(get_db), data: OAuth2PasswordRequestForm = Depends()):
+    user = get_user_by_username(db=db, username=data.username)
     if user:
-        is_password_correct = verify_password(password, user.password)
+        is_password_correct = verify_password(data.password, user.password)
         if is_password_correct:
-            token = create_access_token(user)
+            token = manager.create_access_token(data={'sub': data.username})
             response = RedirectResponse(router.url_path_for("home"), status_code=status.HTTP_303_SEE_OTHER)
-            response.set_cookie(key="token", value=token)
+            manager.set_cookie(response, token)
             return response
         
         else:
             return templates.TemplateResponse("login.html", {"request": request, "bad_password": True})
     
     return templates.TemplateResponse("register.html", {"request": request, "exists": False})
+
+# TODO make the database relational now that the login works
+# TODO add some error messages to the login/register forms
+# TODO dockerize the app
+# TODO maybe improve the forms visualization (thats not a primary goal right now)
